c8acf0e3a1e999fe77099d83c16b997c62c98545
Joshua Noble <joshuajnoble@gmail.com>
2013-11-20 21:34:30 -0800

    extraneous debug code removal

--- send/send.ino
+++ send/send.ino
@@ -12,38 +12,29 @@ unsigned char data[1];
 void setup() 
 {  
   pinMode(HALL_PIN, INPUT);
-  pinMode(1, OUTPUT);
   MANCHESTER.SetTxPin(4);
-  
-  digitalWrite(1, LOW);
 }
 
 void loop()
 {
  
   int hall = analogRead(HALL_PIN);
-  
-//  Serial.println(hall);
    
   if(!occupied) { 
     if(hall < 200 || hall > 800) // either side of the magnetic field
     {
-      uint16_t m = 1;
       data[0] = 2;
       MANCHESTER.TransmitBytes(1, &data[0]);
       occupied = true;
-      digitalWrite(1, HIGH);
     }
   }
   else // we're occupied
   {
     if(hall > 200 && hall < 800) // either side of the magnetic field
     {
-      uint16_t m = 0;
       data[0] = 1;
       MANCHESTER.TransmitBytes(1, &data[0]);
       occupied = false;
-      digitalWrite(1, LOW);
     }
   }
   
